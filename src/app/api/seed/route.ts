f9a211239361355b5c5c95b892941b02942f0624
Daniel Gwilt <daniel.gwilt.software@gmail.com>
2025-08-13 16:16:32 -0700

    Add real DB connection

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,9 +1,18 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
+import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import * as schema from '../../../db/schema';
 
 export async function POST() {
-  const records = await db.insert(advocates).values(advocateData).returning();
+  if(isRealDb(db)) {
+    const records = await db.insert(advocates).values(advocateData).returning();
+    return Response.json({ advocates: records });
+  } else {
+    return Response.error();
+  }  
+}
 
-  return Response.json({ advocates: records });
+function isRealDb(db: unknown): db is PostgresJsDatabase<typeof schema> {
+  return !!db && typeof(db as any).insert === 'function';
 }
